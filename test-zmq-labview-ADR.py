137c67ab3f710696df237337c15a271a8a3ca5fb
Antonino Miceli <antoninomiceli@users.noreply.github.com>
2017-09-07 15:18:52 -0500

    Initial commit

---
+++ test-zmq-labview-ADR.py
@@ -0,0 +1,20 @@
+import sys
+import zmq
+import time
+
+port = "5555"
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+#denali2.xray.aps.anl.gov: 164.54.101.7
+socket.connect ("tcp://164.54.101.7:%s" % port)
+
+socket.setsockopt(zmq.SUBSCRIBE, "")
+
+while(1):
+    message = socket.recv()
+    print "ADR 50mK temp: %s K" % message
+    time.sleep(1)
+
